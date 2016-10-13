f11729d677791823333f4ea97c56ab0527f33861
baltanlaboratories <marlou@baltanlaboratories.org>
2016-10-13 15:21:26 +0200

    Add files via upload

---
+++ Source-folder-DVD/eeg-kiss/Source/EEG-KISS-Project/root/version.py
@@ -0,0 +1,15 @@
+class Version( object ):
+
+    def __init__( self ):
+        self.MajorVersionNumber = 1
+        self.MinorVersionNumber = 6
+        self.BuildNumber = 0
+    
+    def getVersionNumber( self ):
+        return str( self.MajorVersionNumber ) + '.' + str( self.MinorVersionNumber )
+    
+    def getVerboseVersionNumber( self ):
+        return str( self.MajorVersionNumber ) + '.' + str( self.MinorVersionNumber ) + '.' + str( self.BuildNumber )
+    
+    def getBuildNumber( self ):
+        return str( self.BuildNumber )
